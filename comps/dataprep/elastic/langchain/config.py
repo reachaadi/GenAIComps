c5bb728adae05ef06acf3ea15d43d1f3470854d2
Adarsh <reachaadi@gmail.com>
2024-11-21 07:50:00 +0530

    Elastic ingestion

--- comps/dataprep/elastic/langchain/config.py
+++ comps/dataprep/elastic/langchain/config.py
@@ -1,6 +1,7 @@
 import os
 
-ES_CONNECTION_STRING = os.getenv("ES_CONNECTION_STRING", "localhost")
+ES_CONNECTION_STRING = os.getenv("ES_CONNECTION_STRING", "http://localhost:9200")
+UPLOADED_FILES_PATH = os.getenv("UPLOADED_FILES_PATH", "./uploaded_files/")
 
 # Embedding model
 EMBED_MODEL = os.getenv("EMBED_MODEL", "BAAI/bge-base-en-v1.5")
@@ -15,5 +16,5 @@ INDEX_NAME = os.getenv("INDEX_NAME", "rag-elastic")
 CHUNK_SIZE = os.getenv("CHUNK_SIZE", 1500)
 CHUNK_OVERLAP = os.getenv("CHUNK_OVERLAP", 100)
 
-current_file_path = os.path.abspath(__file__)
-parent_dir = os.path.dirname(current_file_path)
+# Logging enabled
+LOG_FLAG = os.getenv("LOGFLAG", False)
